9b6786ed430279e659404435c41e873c835cf2f4
a-laarabi <contact.laarabi@gmail.com>
2022-09-23 05:30:01 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -33,3 +33,29 @@ ALTER TABLE animals
 
 ALTER TABLE animals
     ADD owner_id INT REFERENCES owners(id);
+
+
+-- IV Vet clinic database: add "join table" for visits
+
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    age INT,
+    date_of_graduation DATE
+);
+
+ALTER TABLE vets
+    ADD PRIMARY KEY (id);
+
+CREATE TABLE specializations (
+    id_species INT REFERENCES species(id),
+    id_vets INT REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    id_animals INT REFERENCES animals(id),
+    id_vets INT REFERENCES vets(id),
+    date_of_visit DATE,
+    PRIMARY KEY (id)
+);
